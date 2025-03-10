b903beb587d2616da8e41611d6c12298e23778e0
Thiago <teagos371@gmail.com>
2025-03-09 22:40:34 -0300

    VTK-11 - Criado projeto Spring (/backend) e Angular (/frontend)

---
+++ VortekPonto/backend/src/main/java/vortek/sistponto/SistpontoApplication.java
@@ -0,0 +1,13 @@
+package vortek.sistponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistpontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistpontoApplication.class, args);
+	}
+
+}
---
+++ VortekPonto/backend/src/test/java/vortek/sistponto/SistpontoApplicationTests.java
@@ -0,0 +1,13 @@
+package vortek.sistponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistpontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
